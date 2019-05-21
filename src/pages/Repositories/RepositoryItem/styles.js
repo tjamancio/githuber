d97582dca8dd2c13750547dc0e7f2964c13ad66e
Thiago Amâncio <tjamancio@censanet.com.br>
2019-05-21 15:09:27 -0300

    App

---
+++ src/pages/Repositories/RepositoryItem/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+import { metrics, colors } from '~/styles';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    flex: 1,
+    marginHorizontal: metrics.baseMargin * 2,
+    marginTop: metrics.baseMargin,
+    padding: metrics.basePadding,
+  },
+
+  info: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginRight: metrics.baseMargin,
+  },
+
+  infoContainer: {
+    flexDirection: 'row',
+    marginTop: metrics.baseMargin,
+  },
+
+  infoIcon: {
+    color: colors.dark,
+  },
+
+  infoText: {
+    color: colors.dark,
+    fontSize: 12,
+    marginLeft: metrics.baseMargin / 2,
+  },
+
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
